dcc44ad0a8f006102510d711bc923f588bab9f76
Joe Karasek <joseph.karasek@gmail.com>
2016-05-01 11:17:30 -0700

    ball doesn't 'sink' into walls any more

--- scripts.js
+++ scripts.js
@@ -5,20 +5,31 @@ $(document).ready(function() {
   var y = canvas.height-30;
   var dx = 2;
   var dy = -2;
+  var ballRadius = 10;
 
 
   function drawBall() {
     // Draw the ball
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
   }
+
   function draw() {
     // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    // Detect Collisions
+    // Bounce off the top and bottom
+    if(y + dy > canvas.height || y + dy < 0) {
+      dy = -dy;
+    }
+    // Bounce off the left and righ walls
+    if(x + dx > canvas.width || x + dx < 0) {
+      dx = -dx;
+    }
     // Change the balls position
     x += dx;
     y += dy;
